a63f41f35c4498dc5b28cf597ad7fa4246bcdbdc
notYoosung <98567880+notYoosung@users.noreply.github.com>
2023-02-12 17:17:55 -0600

    Added `settings`, `grid`, grid population (w/ `settings.gridSize`), `setGrid`, `getGrid`, and some optimizations for the KA evoronment.

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
+/* *///
+(function() {
+    var window = this;
+    if (!window.__disableLoopProtection__) {
+        window.__disableLoopProtection__ = true;
+        window.ASTTransforms.rewriteNewExpressions = function() {
+            return {};
+        };
+        window.LoopProtector.prototype.leave = function() { };
+        window.PJSCodeInjector.prototype.exec = function(code, context, mutatingCalls) {
+            if (!code) {
+                return;
+            }
+            try {
+                code = '(function() {' + code + '})();';
+                var transformedCode = this.transformCode(code, context, mutatingCalls);
+                transformedCode = transformedCode.slice(14, -5);
+                var funcBody = 'var ' + this.envName + ' = context;\n' + transformedCode;
+                var func = new window.Function("context", funcBody);
+                func(context);
+            } catch (e) {
+                return e;
+            }
+        };
+        var env = window.Processing.instances[0];
+        env.Program.restart();
+        throw new window.Error("Restarting program");
+    }
+})();
+smooth();
+
+var settings = {
+    gridSize: 128,
+};
+
+var grid = [];
+for (var i = 0; i < settings.gridSize; i++) {
+    grid[i] = [];
+    for (var j = 0; j < settings.gridSize; j++) {
+        grid[i][j] = ' ';
+    }
+}
+
+function setGrid(x, y, setTo) {
+    grid[y][x] = setTo;
+}
+function getGrid(x, y) {
+    return grid[y][x];
+}
+setGrid(0, 0, '1');
+println(getGrid(0, 0));
+
+
+void (1 || random());
 
