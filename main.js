9a8fa97351a736c48a1dda44269b10f84a252172
notYoosung <98567880+notYoosung@users.noreply.github.com>
2023-02-12 17:59:19 -0600

    Added some useful functions. In `cam`, addded offset values and `updateCam`. Added `gridToCart`. Had to use `Object.keys` to be able to enumerate through negative values in `grid`.

--- main.js
+++ main.js
@@ -28,9 +28,47 @@ void (/\/*/);
     }
 })();
 smooth();
+angleMode = "radians";
+
+Object.constructor.prototype.new = (function() {
+    var obj = Object.create(this.prototype);
+    this.apply(obj, arguments);
+    return obj;
+});
+
+function isThing(something) {
+    return something !== undefined && something !== null;
+}
+
+Math.TAU = Math.PI * 2;
+Math.dist = function(x1, y1, x2, y2, dst) {
+    return dst ?
+        (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) <= dst * dst) :
+        Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+};
+Math.distSq = function(x1, y1, x2, y2) {
+    return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+};
+function hasProp(obj, prop) {
+    return obj.hasOwnProperty(prop);
+}
+Object.constructor.prototype.hasProp = hasProp;
+
+function getKeys(obj) {
+    return Object.keys(obj);
+}
+
+function getValues(obj) {
+    return Object.values(obj);
+}
+
+function getEntries(obj) {
+    return Object.entries(obj);
+}
+
 
 var settings = {
-    gridSize: 128,
+    unitSize: 10,
 };
 
 
@@ -41,17 +79,35 @@ var cam = (function() {
     var cam = {
         internals: {
             x: 0,
-            y: 0
+            y: 0,
+            currX: 0,
+            currY: 0,
         },
+        offsetX: 300,
+        offsetY: 300,
         set x(val) {
             this.internals.x = val;
         },
         get x() {
             return this.internals.x;
-        }
-    };
-})();
+        },
+        updateCam: function() {
+            var x = this.internals.x;
+            var y = this.internals.y;
+            var currX = this.internals.currX;
+            var currY = this.internals.currY;
+            var moveDir = Math.atan2(y - currY, x - currX),
+                moveMag = Math.dist(currX, currY, x, y);
+            this.internals.x += Math.cos(moveDir) * moveMag;
+            this.internals.y += Math.sin(moveDir) * moveMag;
 
+            this.offsetX = this.internals.currX + 300;
+            this.offsetY = this.internals.currY + 300;
+
+        },
+    };
+    return cam;
+})();
 
 /**
  * Grid/map
@@ -67,9 +123,22 @@ function setGrid(x, y, setTo) {
 function getGrid(x, y) {
     return grid[y][x];
 }
-setGrid(-10, 0, '1');
-println(getGrid(-10, 0));
+function gridToCart(x, y) {
+    return {
+        x: (settings.unitSize * x),
+        y: (settings.unitSize * y)
+    };
+}
 
+setGrid(-10, -2, '1');
+Object.keys(grid).forEach(function(yPos) {
+    Object.keys(grid[yPos]).forEach(function(xPos) {
+        println(getGrid(xPos, yPos));
+    });
+});
 
-void (1 || random());
+draw = function() {
+    background(255);
+    ellipse(cam.offsetX, cam.offsetY, 10, 10);
+};
 
