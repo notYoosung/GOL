9d40096272a4b51826548ba0c6ba75aff4d590d2
notYoosung <98567880+notYoosung@users.noreply.github.com>
2023-02-13 09:10:45 -0600

    Added `mouse`, an IIFE for `keys` and `mounse`, and formatted the text to align @ the center and not turn white.

--- main.js
+++ main.js
@@ -60,6 +60,62 @@ var keys = (function() {
     return keys;
 })();
 
+var mouse = (function() {
+    var mouse = {
+        clicked: false, // mouseClicked
+        clickActive: false, // If a click has been used
+        initialPressed: false, // 
+        initialPressedPos: [undefined, undefined],
+        down: false,
+        up: true,
+        pressed: false, // mousePressed
+        released: false, // mouseReleased
+        dragged: false, // mouseDragged
+        out: true, // mouseOut
+        availableClick: true, // 
+        focused: focused, // Is the mouse focused on the canvas
+
+        doubleClicked: false, // isDoubleClicked
+        startDoubleClickTimer: false, // If to start the timer
+        doubleClickTimer: 0, // The timer
+        resetDoubleClick: function() {
+            mouse.startDoubleClickTimer = false;
+            mouse.doubleClickTimer = 0;
+        }, // To reset the properties
+    };
+    mouseClicked = function() {
+        mouse.clicked = !false;
+        (function /*isDoubleClicked*/() {
+            if (mouse.doubleClickTimer >= 0 && mouse.startDoubleClickTimer) {
+                mouse.doubleClicked = true;
+                mouse.resetDoubleClick();
+            }
+            if (!mouse.startDoubleClickTimer) {
+                mouse.startDoubleClickTimer = true;
+                mouse.doubleClickTimer = 20;
+            }
+        })();
+    };
+    mousePressed = function() {
+        mouse.initialPressed = true;
+        mouse.initialPressedPos = [mouseX, mouseY];
+        mouse.pressed = true;
+    };
+    mouseReleased = function() {
+        mouse.pressed = false;
+        mouse.released = true;
+    };
+    mouseDragged = function() {
+        mouse.dragged = true;
+    };
+    mouseOver = function() {
+        mouse.over = true;
+    };
+    mouseOut = function() {
+        mouse.over = false;
+    };
+    return mouse;
+})();
 
 function isThing(something) {
     return something !== undefined && something !== null;
@@ -103,28 +159,53 @@ var settings = {
 var cam = (function() {
     var cam = {
         internals: {
-            x: 0,
-            y: 0,
+            x: 100,
+            y: 100,
             currX: 0,
             currY: 0,
         },
         offsetX: 300,
         offsetY: 300,
+        speed: 10,
         set x(val) {
+            // println(val);
             this.internals.x = val;
         },
         get x() {
             return this.internals.x;
         },
+        set y(val) {
+            // println(val);
+            this.internals.y = val;
+        },
+        get y() {
+            return this.internals.y;
+        },
         updateCam: function() {
+            if (keys[LEFT]) {
+                cam.x -= cam.speed;
+            }
+            if (keys[RIGHT]) {
+                cam.x += cam.speed;
+            }
+            if (keys[UP]) {
+                cam.y -= cam.speed;
+            }
+            if (keys[DOWN]) {
+                cam.y += cam.speed;
+            }
+
+
+
             var x = this.internals.x;
             var y = this.internals.y;
             var currX = this.internals.currX;
             var currY = this.internals.currY;
             var moveDir = Math.atan2(y - currY, x - currX),
                 moveMag = Math.dist(currX, currY, x, y);
-            this.internals.x += Math.cos(moveDir) * moveMag;
-            this.internals.y += Math.sin(moveDir) * moveMag;
+            var lerpMag = (moveMag * 0.10);
+            this.internals.currX += Math.cos(moveDir) * lerpMag;
+            this.internals.currY += Math.sin(moveDir) * lerpMag;
 
             this.offsetX = this.internals.currX + 300;
             this.offsetY = this.internals.currY + 300;
@@ -156,25 +237,37 @@ function gridToCart(x, y) {
 }
 
 textSize(settings.unitSize);
+textAlign(CENTER, CENTER);
 fill(0);
 setGrid(-10, -2, '1');
-setGrid(10, 2, '1');
+setGrid(10, 2, '2');
+setGrid(0, 0, '0');
 
 draw = function() {
     background(255);
 
-    if (keys[LEFT]) {
-        cam.x = cam.x - 1;
-    }
-
     cam.updateCam();
 
+    fill(0);
     Object.keys(grid).forEach(function(yPos) {
         Object.keys(grid[yPos]).forEach(function(xPos) {
             text(grid[yPos][xPos], xPos * settings.unitSize + cam.offsetX, yPos * settings.unitSize + cam.offsetY);
         });
     });
 
+    (function /*resetVariables*/() {
+        mouse.initialPressed = mouse.doubleClicked = mouse.pressed = mouse.released = mouse.clicked = mouse.dragged = false;
+        keys.pressed = keys.released = keys.pressedCode = keys.pressedKey = keys.releasedCode = keys.releasedKey = false;
+        mouse.focused = focused;
+
+        if (mouse.startDoubleClickTimer) {
+            mouse.doubleClickTimer--;
+
+            if (mouse.doubleClickTimer < 0) {
+                mouse.resetDoubleClick();
+            }
+        }
+    })();
 };
 
 void (1 || random());
